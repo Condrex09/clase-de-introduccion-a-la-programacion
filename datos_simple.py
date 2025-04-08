fea7322e5fbcfbba849e3c378cc603a87545869c
CONDREX <benjapaifre@gmail.com>
2025-04-08 18:20:42 -0400

    lol

---
+++ datos_simple.py
@@ -0,0 +1,38 @@
+# Datos de tipo STRING o cadenas de texto
+nombre = "Benjamin Gomez"
+asignatura = "Introduccion a la programacion"
+
+variable1 = '''Introduccion a la
+programacion'''
+
+print(variable1)
+print(type(variable1))
+
+# Datos de tipo numericos enteros INT y decimales FLOAT
+# numero1 = 49
+# numero2 = 49.5
+
+# print(numero1)
+# print(type(numero1))
+
+# print(numero2)
+# print(type(numero2))
+
+# Datos de valor logico TRUE FALSE o dato BOOLEAND BOOL
+# verdadero = True
+
+# print(verdadero)
+# print(type(verdadero))
+
+# nombre = "Benjamin"
+# apellido = "Gomez"
+# edad = 19
+#print(nombre + apellido)
+#print(nombre, apellido)
+#print("saludos", nombre, apellido, ". Su edad es", edad, ".")
+#print("saludos " + nombre + " " + ". Su edad es " + str(edad) + ".")
+
+# numero_1 = 35
+# numero_2 = "44"
+# resultado = numero_1 + int(numero_2)
+# print(resultado)
