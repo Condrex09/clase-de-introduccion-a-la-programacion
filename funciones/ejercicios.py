0f7d9b320032e315d9ecfed01fcbb49cc464d188
CONDREX <benjapaifre@gmail.com>
2025-04-17 19:06:05 -0400

    ALO

---
+++ funciones/ejercicios.py
@@ -0,0 +1,7 @@
+import funciones.calculadora
+
+numero_1 = float(input("ingrese su primer numero: "))
+numero_2 = float(input("ingrese su segundo numero: "))
+operacion = input("ingrese su operacion: ")
+
+funciones.calculadora(numero_1,numero_2,operacion)
