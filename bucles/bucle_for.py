e9e30b12f621d4b84b19214b3a2f8a26f7f3e5fb
CONDREX <benjapaifre@gmail.com>
2025-04-17 18:21:04 -0400

    hola

--- bucles/bucle_for.py
+++ bucles/bucle_for.py
@@ -1,6 +1,12 @@
 # Usamos el ciclo FOR para recorrer elementos de un grupo de datos
 juegos = ["Dota 2", "MK", "Street Fighter", "Counter Strike", "7DaystoDie"]
 numeros = [10,20,30,40,50]
+diccionario = {
+    "nombre":"Benjamin",
+    "apellido":"Gomez",
+    "edad":19,
+    "estudiante":False
+}
 
 for juego in juegos:
     print(juego)
@@ -18,3 +24,20 @@ for elemento in enumerate(numeros):
     indice = elemento[0]
     valor = elemento[1]
     print(f"El indice es: {indice} y el valor es: {valor}")
+
+print()
+for elemento in diccionario:
+    print(f"la clave del dato es: {elemento}")
+
+print()
+for elemento in diccionario.items():
+    clave = elemento[0]
+    valor = elemento[1]
+    print(f"la clave del dato es: ´{clave}´ y el valor es: ´{valor}´")
+
+conjunto = {"Aquiles Baeza", 55,True,"commandos",23}
+
+print()
+for elemento in conjunto:
+    if type(elemento) == str:
+        print(elemento)
---
+++ bucles/funciones/funciones.py
@@ -0,0 +1,5 @@
+def saludar(nombre):
+    print(f"buen dia estimado(a) {nombre}")
+
+nombre = input("ingrese su nombre: ")
+saludar(nombre)
